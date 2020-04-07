1a3712e9f990aaf44abf326b83710b0d9120fe8c
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-07 18:49:47 +0000

    Add functionality to save data to file

--- directory.rb
+++ directory.rb
@@ -1,5 +1,15 @@
 @students = []
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:country], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   name = gets[0...-1]
@@ -31,6 +41,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 end
 
@@ -46,6 +57,8 @@ def process(selection)
       input_students
     when "2" 
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
