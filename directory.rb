5110928b0415d039cbe0cb9ac37d7689d1f835b5
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-07 13:41:06 +0000

    Refactor by adding methods

--- directory.rb
+++ directory.rb
@@ -1,32 +1,7 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        if !students.empty? 
-          print_header
-          print(students)
-          print_footer(students)
-        else
-          puts "We have no students currently."
-        end
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
-  students = []
   name = gets[0...-1]
   while !name.empty? do
     puts "Please enter country of birth"
@@ -35,38 +10,62 @@ def input_students
     puts "To finish, just hit return twice"
     cohort = gets.chomp.to_s
     if country.empty? 
-      country = "N/A"
     end
     if cohort.empty?
       cohort = "N/A"
     end
-    students << {name: name, country: country, cohort: cohort}
-    students.count == 1 ? studentWord = "student" : studentWord = "students"
-    puts "Now we have #{students.count} #{studentWord}"
+    @students << {name: name, country: country, cohort: cohort}
+    @students.count == 1 ? studentWord = "student" : studentWord = "students"
+    puts "Now we have #{@students.count} #{studentWord}"
     name = gets.chomp
   end
-  students
 end
 
-# Print the list of students
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end 
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2" 
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy".center(45)
   puts "-------------".center(45)
 end
 
-def print(students)
-  n = 0
-  while n < students.length
-    puts "#{students[n][:name].center(15)} #{students[n][:country]} (cohort: #{students[n][:cohort]})"
-    n += 1
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name].center(15)} #{student[:country]} (cohort: #{student[:cohort]})"
   end
 end
 
-def print_footer(students)
-# print the total number of students
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
-
-
